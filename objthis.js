26731be1fa16fe6aa2ee09e80759656ded39f575
Avi7877489 <avi7877489@gmail.com>
2024-10-25 15:32:37 +0530

    by defult print vlue

--- objthis.js
+++ objthis.js
@@ -1,10 +1,17 @@
 let obj ={
     firstname: 'Avijit',
     lastname: 'Ghosh',
-    fullname: function(){
-        return this.firstname + ' ' + this.lastname
+    fullname: function(first,last){
+
+        if(first == undefined || last == undefined){
+        return this.firstname + ' ' + this.lastname   // print under onj variable by defualt
+    }else{
+        return first + ' ' + last // print user input
+
+    }
     }
 
 
 }
 console.log(obj.fullname())
+console.log(obj.fullname('jagat','pal'))
