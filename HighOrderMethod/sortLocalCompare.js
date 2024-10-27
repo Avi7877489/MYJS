19148872f3d17a4590537990edc6ee778b13d350
Avi7877489 <avi7877489@gmail.com>
2024-10-28 00:15:28 +0530

    Sort String Using LocalCompare

---
+++ HighOrderMethod/sortLocalCompare.js
@@ -0,0 +1,34 @@
+let product = [
+    {
+        title : "Samsung 1" , 
+        price : 1200 
+    } , 
+    {
+        title : "apple 1" , 
+        price : 1400 
+    } , 
+    {
+        title : "nokia 1" , 
+        price : 800 
+    } ,
+    {
+        title : "onePlus 1" , 
+        price : 1100 
+    } , 
+    {
+        title : "Motorola 1" , 
+        price : 1300 
+    }
+]
+
+
+let narr = product.sort((a,b)=>{
+    return  a.title.localeCompare(b.title);
+})
+
+
+
+
+
+console.log(narr)
+
