f8d667b37c4f0bf8b4259eaf301311dc7ad2e4ad
Avi7877489 <avi7877489@gmail.com>
2024-10-30 23:15:11 +0530

    add Quize function

--- Closures/Encapsulation.js
+++ Closures/Encapsulation.js
@@ -10,22 +10,45 @@
 //   counter();
   
 
-  function counter(){
-    let count = 0
-    return {
-        increment: function(){
-            count++
-            return count
-        },
-        decrimint: function(){
-            count--
-            return count
-        }
+  // function counter(){
+  //   let count = 0
+  //   return {
+  //       increment: function(){
+  //           count++
+  //           return count
+  //       },
+  //       decrimint: function(){
+  //           count--
+  //           return count
+  //       }
+  //   }
+  // }
+  // let countersetup = counter()
+  // console.log(countersetup.increment())
+  // console.log(countersetup.increment())
+  // console.log(countersetup.increment())
+  // console.log(countersetup.increment())
+  // console.log(countersetup.decrimint())
+
+
+  function createAquize(question,ans){
+
+    let attemptcount = 0
+
+    return function(userAns){
+      attemptcount++
+      if(userAns == ans){
+        return 'Well Done' + 'no Of Attempt done' + attemptcount
+      }else{
+        return 'wrong ans' + 'no of attempt done' + attemptcount
+      }
     }
+
+    
+
   }
-  let countersetup = counter()
-  console.log(countersetup.increment())
-  console.log(countersetup.increment())
-  console.log(countersetup.increment())
-  console.log(countersetup.increment())
-  console.log(countersetup.decrimint())
+
+  let quiz1 = createAquize('Where does sun rice', 'east')
+  console.log(quiz1('north'))
+  console.log(quiz1('east'))
+
